ee59d8812ccf9f84a5384fbd4dd5318415c57bd5
hhkaka <151792447@qq.com>
2018-03-05 11:16:52 +0800

    缓存第3章总结
    缓存第3章总结

--- progressive-times/js/article.js
+++ progressive-times/js/article.js
@@ -1,16 +1,16 @@
-function retrieveData(url) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var result = JSON.parse(this.responseText);
-        buildWebPage(result);
+function retrieveData(url) {                             //retrieveData数据检索
+  var xhttp = new XMLHttpRequest();                      //XML HTTP请求
+  xhttp.onreadystatechange = function() {               //就绪状态变化
+    if (this.readyState == 4 && this.status == 200) {    //就绪状态  status地位;身份;情形，状态
+        var result = JSON.parse(this.responseText);      //result结果   responseText这个响应文本
+        buildWebPage(result);                            // buildWebPage建Web页面
     }
   };
   xhttp.open('GET', url, true);
-  xhttp.send();
+  xhttp.send();   //send发送
 }
 
-// Get a value from the querystring
+// Get a value from the querystring   从查询获得的价值
 function findGetParameter(parameterName) {
     var result = null,
         tmp = [];
@@ -22,15 +22,15 @@ function findGetParameter(parameterName) {
     return result;
 }
 
-// Load the article contents into the page
+// Load the article contents into the page  将文章内容加载到页面中
 function loadArticle(){
-  // Get the details for the article
+  // Get the details for the article 获取文章的详细信息
   var articleId = findGetParameter('id');
   var articleUrl = './data/data-' + articleId + '.json';
   retrieveData(articleUrl);
 }
 
-// Build the web page with the resulting data
+// Build the web page with the resulting data 用结果数据构建Web页面
 function buildWebPage(result){
   document.getElementById('article').innerHTML = result.description;
   document.getElementById('article-title').innerHTML = result.title;
